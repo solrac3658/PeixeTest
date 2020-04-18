15f9e19ff90cf6b208d75db1eb37a8130d949278
Solrac3658 <carlos.daniel.009@hotmail.com>
2020-04-18 19:13:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = '29ccffe5';
+const URL = "http://www.omdbapi.com/"
+class NameForm extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {MovieName: ''};
+    
+     this.changeinput = this.changeinput.bind(this);
+     this.buscar= this.buscar.bind(this)
+  }
+
+
+  changeinput(event){
+    this.setState({MovieName: event.target.value});
+  
+  }
+
+  buscar(){
+    alert("http://www.omdbapi.com/?s="+this.state.MovieName+"&apikey="+API_KEY);
+
+    fetch(`http://www.omdbapi.com/?s=batman&apikey=29ccffe5`)
+    .then(resp => resp)
+    .then(resp => resp.json())
+    .then(response => {
+        if (response.Response === 'False') {
+            alert(response.Error);
+            console.log(response.Error)
+        }
+        else {
+            console.log(response.Search);
+        }
+
+    })
+    .catch(({message}) => {
+        alert(message);
+        console.log(message)
+    })
+
+  }
+
+   render() {
+    return (
+      <div className="App">
+        Realizando pruebas
+         <Form>
+            <Form.Group controlId="formBasictext">
+              <Form.Label>Nombre de Película</Form.Label>
+              <Form.Control type="text" value ={this.state.MovieName} placeholder="Ingrese Nombre de Película" onChange={this.changeinput} />
+            </Form.Group>
+
+            <Button variant="primary" type="button" onClick = {this.buscar}>
+              Buscar
+            </Button>
+        </Form>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default NameForm;
