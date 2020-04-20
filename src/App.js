d51d8dac7750d74aea74c4acdd159d3a759de9f3
Solrac3658 <carlos.daniel.009@hotmail.com>
2020-04-19 22:44:08 -0400

    Mejoras Front

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Form, Button, Table, Modal, Container, Row, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import ReactTooltip from "react-tooltip";
 
 
 const API_KEY = '29ccffe5';
@@ -44,11 +45,9 @@ class PeixeTest extends React.Component {
     this.getRowsData = this.getRowsData.bind(this);
   }
 
-
   changeInput(event) {
     this.setState({MovieName: event.target.value});
   }
-
   hideShow() {
       this.setState({Show: false});
   }
@@ -62,7 +61,7 @@ class PeixeTest extends React.Component {
         alert(MovieData.Error);
         console.log(MovieData.Error)
       }else {
-        if (this.state.Favorites.filter((element) => {return element.Title === input.Title}).length == 1){
+        if (this.state.Favorites.filter((element) => {return element.Title === input.Title}).length === 1){
             this.setState({StarColor : "#ffff00",
                             InFavorites: true})
         }else{
@@ -128,23 +127,23 @@ class PeixeTest extends React.Component {
     }
   }
 
-  getHeader = function(){
+  getHeader(){
     return (
       <>
-        <th> Title </th>
-        <th> Year </th> 
         <th> Poster </th>
+        <th> Title </th> 
+        <th> Year </th>
       </>
     );
   }
    
-  getRowsData = function(){ 
+  getRowsData(){ 
     return( this.state.TableData.map(( value) => {
       return (
         <tr onClick={() => this.movieDescription(value)}>
-          <td> {value.Title} </td> 
-          <td> {value.Year} </td>
-          <td> <img src={value.Poster} /> </td> 
+          <td> <img class="image_find" src={value.Poster} /> </td>
+          <td align="center"> {value.Title} </td> 
+          <td> {value.Year} </td> 
         </tr>) 
       })
     );
@@ -155,13 +154,17 @@ class PeixeTest extends React.Component {
       <div className="App">
         <Container>
           <Form>
-            <Form.Group controlId="formBasictext">
-              <Form.Label>Nombre de Película</Form.Label>
-              <Form.Control type="text" value ={this.state.MovieName} placeholder="Ingrese Nombre de Película" onChange={this.changeInput} />
-            </Form.Group>
-            <Button variant="primary" type="button" onClick={this.find}> Buscar </Button>
-            <Button variant="primary" type="button" onClick={this.favorites}> favoritos </Button>
+            <Row>
+              <Col lg={10}>
+                <Form.Group controlId="formBasictext">
+                  <Form.Control type="text" value ={this.state.MovieName} placeholder="Ingrese Nombre de Película" onChange={this.changeInput}/>
+                </Form.Group>
+              </Col>
+              <Col lg={1}> <Button variant="outline-success"  type="button" onClick={this.find}> Buscar </Button> </Col>
+              <Col lg={1}> <Button variant="outline-warning" type="button" onClick={this.favorites}> Favoritos </Button> </Col>
+            </Row>
           </Form>
+          
           <div>
             <Table>
               <thead>
@@ -172,33 +175,35 @@ class PeixeTest extends React.Component {
               </tbody>
             </Table>
           </div>
-          <Modal show={this.state.Show}>
-            <Modal.Header closeButton onClick={this.hideShow}>
-                <Modal.Title> {this.state.Title} </Modal.Title>
+          
+          <Modal size="lg" show={this.state.Show}>
+            <Modal.Header>
+              <Modal.Title> {this.state.Title} </Modal.Title>
+              <FontAwesomeIcon id="button_favorite"  data-tip="Favoritos" icon={faStar} onClick={this.addFavorites} style={{color: this.state.StarColor}}/>
+              <ReactTooltip />              
             </Modal.Header>
             <Modal.Body> 
               <Row>
-                <Col sm={5}>
-                  <img src={this.state.Poster} />
-                </Col>
-                <Col sm={7}>
-                  <p> Rated: {this.state.Rated} </p> 
-                  <p> Released: {this.state.Released} </p>
-                  <p> Duration: {this.state.RunTime} </p>
-                  <p> Genere: {this.state.Genre} </p>
-                  <p> Director: {this.state.Director} </p>
-                  <p> Writer: {this.state.Writer} </p>
-                  <p> Actors: {this.state.Actors} </p>
-                  <p> Plot: {this.state.Plot} </p>
-                  <p> Language: {this.state.Language} </p>
-                  <p> Country: {this.state.Country} </p>
-                  <p> Awards: {this.state.Awards} </p>
+                <Col lg={5}> <img id="image_modal" src={this.state.Poster} /> </Col>
+                <Col lg={7}>
+                  <h6> <b> Plot: </b> </h6> <p> {this.state.Plot} </p>  
+                  <h6><b> Director: </b> </h6> <p> {this.state.Director} </p> 
+                  <h6>  <b> Actors: </b> </h6> <p> {this.state.Actors} </p>
+                  <h6><b> Writer: </b> </h6> <p> {this.state.Writer} </p>
                 </Col>
               </Row>
+              <Row> 
+                <Col lg={2}> <br/> <b> Rated: </b> <p>{this.state.Rated} </p> </Col> 
+                <Col lg={2}> <br/> <b> Released: </b> <p>{this.state.Released} </p> </Col>
+                <Col lg={2}> <br/> <b> Duration: </b> <p>{this.state.RunTime} </p> </Col> 
+                <Col lg={2}> <br/> <b> Genere: </b> <p>{this.state.Genre} </p> </Col> 
+                <Col lg={2}> <br/> <b> Language: </b> <p>{this.state.Language} </p> </Col> 
+                <Col lg={2}> <br/> <b> Country: </b> <p>{this.state.Country} </p> </Col> 
+              </Row>
+              <b> Awards: </b> <p>{this.state.Awards} </p>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={this.hideShow}> Close </Button>
-              <FontAwesomeIcon icon={faStar} onClick={this.addFavorites} style={{ color: this.state.StarColor }}  />
+              <Button variant="outline-danger" onClick={this.hideShow}> Close </Button>
             </Modal.Footer>
           </Modal>
         </Container>
